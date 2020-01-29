5fbde762ad8a26a94e099b2ef43716d4aeae8410
Souvik Biswas <sbis1999@gmail.com>
2020-01-29 08:17:35 +0530

    Updated the project to the latest version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/souvikbiswas/Documents/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/souvikbiswas/Desktop/Flutter_Resource/Medium Article Apps/sign_in_flutter"
+export "FLUTTER_ROOT=/Users/souvikbiswas/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/souvikbiswas/Mobile Apps/Flutter/sign_in_flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/souvikbiswas/Documents/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/souvikbiswas/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
